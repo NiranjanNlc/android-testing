5b36d375bdd3af9ade53ef62bc4827180e961260
niranjannlc <niranjannlc10@gmail.com>
2022-02-21 13:52:21 +0545

    failed live data testing in viewmodal

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -4,7 +4,11 @@ import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import junit.framework.TestCase
+import org.hamcrest.CoreMatchers.not
+import org.hamcrest.core.IsNull.nullValue
+import org.junit.Assert.assertThat
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
@@ -14,10 +18,13 @@ class TasksViewModelTest : TestCase() {
     var instantExecutorRule = InstantTaskExecutorRule()
 
 
+    @Test
     fun testAddNewTask() {
          // Given a fresh ViewModel
         val tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
         // When adding a new task
         tasksViewModel.addNewTask()
+        val value = tasksViewModel.newTaskEvent.getOrAwaitValue()
+        assertEquals(value.getContentIfNotHandled(), (not(nullValue())))
     }
 }
