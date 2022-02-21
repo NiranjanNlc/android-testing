ea95cd66c6905de7062f170a6e13c1cee295e860
niranjannlc <niranjannlc10@gmail.com>
2022-02-21 13:35:10 +0545

    instant executor rule for arch testing

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -1,12 +1,18 @@
 package com.example.android.architecture.blueprints.todoapp.tasks
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import junit.framework.TestCase
+import org.junit.Rule
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class TasksViewModelTest : TestCase() {
+
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
 
     fun testAddNewTask() {
          // Given a fresh ViewModel
