8315aa3d65bfbebd7795583ab4df0d247131656d
niranjannlc <niranjannlc10@gmail.com>
2022-02-21 13:33:57 +0545

    stttig up roboelctric and and dandroidx test

---
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -0,0 +1,17 @@
+package com.example.android.architecture.blueprints.todoapp.tasks
+
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import junit.framework.TestCase
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class TasksViewModelTest : TestCase() {
+
+    fun testAddNewTask() {
+         // Given a fresh ViewModel
+        val tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+        // When adding a new task
+        tasksViewModel.addNewTask()
+    }
+}
